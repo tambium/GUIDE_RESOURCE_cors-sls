8b8dd674a11c4ad8a4cb336da95787b0f29372ab
Endre Vegh <endre.vegh@yahoo.com>
2020-07-08 21:10:18 +0200

    add basic cors

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ ok: true }),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
   };
 };
