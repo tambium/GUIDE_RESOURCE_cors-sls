f05bd1510da846e81324c75c979e8535fdaa48b9
Endre Vegh <endre.vegh@yahoo.com>
2020-07-21 13:58:14 +0200

    set lambda to allow credentials

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ exports.handler = async () => {
     statusCode: 200,
     body: JSON.stringify({ ok: true }),
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Credentials": true,
     },
   };
 };
