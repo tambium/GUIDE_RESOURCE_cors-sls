f91efaef79bba32b9ec03d3028d45a3e102faae4
Endre Vegh <endre.vegh@yahoo.com>
2020-07-21 16:25:09 +0200

    refactor lambda qith middy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-exports.handler = async () => {
+const middy = require("@middy/core");
+const cors = require("@middy/http-cors");
+
+exports.handler = middy(async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({ ok: true }),
-    headers: {
-      "Access-Control-Allow-Origin": "http://localhost:3000",
-      "Access-Control-Allow-Credentials": true,
-    },
   };
-};
+}).use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
